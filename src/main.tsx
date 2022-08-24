aeb3f857408b4efc29c47e78c572dc86e78fb300
lanmower <almagestfraternite@gmail.com>
2022-08-24 23:49:38 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,2 @@
-const p = (typeof process === 'undefined' ? null : process)
-console.log({
-  process: p,
-  env: p?.env,
-  var: p?.env?.VITE_TEST_VAR,
-})
+declare var process : string | undefined;
+console.log({process})
